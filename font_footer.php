b34ccc230c57d8a2ea6417cf3312c6f71c3b8d11
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-11 23:16:56 +0600

    some text added

--- font_footer.php
+++ font_footer.php
@@ -6,7 +6,7 @@
     <div class="row">
       <div class="col-lg-6">
         <h4>Our Newsletter</h4>
-        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+        <p>Subscribe Our site and get our's latest blog post</p>
       </div>
       <div class="col-lg-6">
         <form action="subscribe.php" method="POST">
