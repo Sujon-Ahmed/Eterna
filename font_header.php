bf8c9be98743aad1f7a18f6894e293bb6083312e
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-01-18 01:03:47 +0600

    active navigation bar useing php_self...

--- font_header.php
+++ font_header.php
@@ -1,59 +1,60 @@
 <?php
-  session_start();
-  include 'admin/main.php';
-  $obj = new Main();
-  // method for fetch email and phone
-  $email = $obj->profile_retrives();
-  if($email->num_rows > 0){
-    while($row = $email->fetch_object()){
-      $admin_email = $row->admin_email;
-      $admin_phone = $row->admin_phone;
-    }
+session_start();
+include 'admin/main.php';
+$obj = new Main();
+// method for fetch email and phone
+$email = $obj->profile_retrives();
+if ($email->num_rows > 0) {
+  while ($row = $email->fetch_object()) {
+    $admin_email = $row->admin_email;
+    $admin_phone = $row->admin_phone;
   }
-  // get all data for banner
-  $banner = $obj->get_banner_data();
-  // card details
-  $card_details = $obj->get_data_limit();
-  // about details
-  $about_details = $obj->get_about_data();
-  // service details
-  $service_details = $obj->get_service_limit();
-  // client description
-  $description = $obj->catch_desc();
-  // client brand
-  $brand = $obj->get_brands();
-  // about page card
-  $abs_card = $obj->get_card_limit();
-  // testimonial section
-  $testimonial = $obj->get_testimonial();
-  // testimonial description
-  $testimonial_description = $obj->get_testimonial_desc_with_limit();
-  // skill description
-  $skill_desc = $obj->get_skill_desc_limit();  
-  // skill content
-  $skill_content = $obj->get_skill_content_limit();
-  // skill progress bar
-  $skill_progress = $obj->get_progress();
-  // team member
-  $team = $obj->get_team_limit();
-  // portfolio section
-  $get_portfolio = $obj->get_port_cat();
-  // portfolio image
-  $get_port_image = $obj->get_port_cat_data();
-  // method for blog category
-  $get_blog_category = $obj->get_cat();
-  // get location
-  $get_location = $obj->get_location();
-  if($get_location->num_rows > 0){
-    while($row = $get_location->fetch_object()){
-      $title = $row->address_title;
-    }
+}
+// get all data for banner
+$banner = $obj->get_banner_data();
+// card details
+$card_details = $obj->get_data_limit();
+// about details
+$about_details = $obj->get_about_data();
+// service details
+$service_details = $obj->get_service_limit();
+// client description
+$description = $obj->catch_desc();
+// client brand
+$brand = $obj->get_brands();
+// about page card
+$abs_card = $obj->get_card_limit();
+// testimonial section
+$testimonial = $obj->get_testimonial();
+// testimonial description
+$testimonial_description = $obj->get_testimonial_desc_with_limit();
+// skill description
+$skill_desc = $obj->get_skill_desc_limit();
+// skill content
+$skill_content = $obj->get_skill_content_limit();
+// skill progress bar
+$skill_progress = $obj->get_progress();
+// team member
+$team = $obj->get_team_limit();
+// portfolio section
+$get_portfolio = $obj->get_port_cat();
+// portfolio image
+$get_port_image = $obj->get_port_cat_data();
+// method for blog category
+$get_blog_category = $obj->get_cat();
+// get location
+$get_location = $obj->get_location();
+if ($get_location->num_rows > 0) {
+  while ($row = $get_location->fetch_object()) {
+    $title = $row->address_title;
   }
-  // get social media
-  $get_social_media = $obj->get_social();
+}
+// get social media
+$get_social_media = $obj->get_social();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -77,15 +78,17 @@
   <!-- <link rel="stylesheet" href="assets/css/all.min.css"> -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <style>
-    .blog-pagination ul li a{
+    .blog-pagination ul li a {
       border: 1px solid #ddd;
     }
-    .blog-pagination ul li a.active{
+
+    .blog-pagination ul li a.active {
       background-color: #e96b56;
       color: #fff;
     }
   </style>
 </head>
+
 <body>
   <!-- ======= Top Bar ======= -->
   <section id="topbar" class="d-flex align-items-center">
@@ -113,14 +116,14 @@
       </div>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="active" href="index.php">Home</a></li>
-          <li><a href="about.php">About</a></li>
-          <li><a href="services.php">Services</a></li>
-          <li><a href="portfolio.php">Portfolio</a></li>
-          <li><a href="team.php">Team</a></li>
-          <li><a href="pricing.php">Pricing</a></li>
-          <li><a href="blog.php">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "index.php") ? "active" : "" ?>" href="index.php">Home</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "about.php") ? "active" : "" ?>" href="about.php">About</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "services.php") ? "active" : "" ?>" href="services.php">Services</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "portfolio.php") ? "active" : "" ?>" href="portfolio.php">Portfolio</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "team.php") ? "active" : "" ?>" href="team.php">Team</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "pricing.php") ? "active" : "" ?>" href="pricing.php">Pricing</a></li>
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "blog.php") ? "active" : "" ?>" href="blog.php">Blog</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
@@ -136,8 +139,8 @@
               <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li>
             </ul>
-          </li>
-          <li><a href="contact.php">Contact</a></li>
+          </li> -->
+          <li><a class="<?= (basename($_SERVER['PHP_SELF']) == "contact.php") ? "active" : "" ?>" href="contact.php">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
